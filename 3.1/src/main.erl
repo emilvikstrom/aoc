13e655cfcfcc227c60fe22ba284b5221639c04ac
emilv <emil.vikstrom@mobilearts.com>
2018-12-07 18:29:34 +0100

    SL coding

--- 3.1/src/main.erl
+++ 3.1/src/main.erl
@@ -4,7 +4,8 @@
 run() ->
     {ok, IODev} = file:open("../data/input", [read] ),
     Values = lists:reverse(handle_input(IODev)),
-    Matrix = build_matrix().
+    Matrix = build_matrix(),
+    place_on_matrix(Values, Matrix) ->
 
 handle_input(IODev) ->
     handle_input(IODev, []).
@@ -14,8 +15,12 @@ handle_input(IODev, Vals) ->
 	    Vals;
 	 {ok, Val} ->
 	    [Id, _, Pos, Size] = string:tokens(Val, " "),
+	    [XSz,YSz] = string:tokens(Size, "x"),
+	    [X,Y] = string:tokens(lists:flatten(string:tokens(Pos,":")),","),
 	    TrimVal = string:trim(Val),
-	    handle_input(IODev,[#{id => Id, pos => Pos, size => Size} | Vals]);
+	    handle_input(IODev,[#{id => Id, 
+				    pos => #{x => X, y => Y }, 
+				    size => #{xsz => XSz, ysz => YSz} } | Vals]);
 	_ ->
 	    {error, unexpected_value}
     end.
@@ -24,4 +29,35 @@ build_matrix() ->
     Cols = lists:flatten(["." || _ <- lists:seq(1,1000)]),
     Matrix = [Cols || _ <- lists:seq(1,1000)].
 
+place_on_matrix([#{id := Id, 
+		    pos := #{x := X, y := Y}, 
+		    size := #{ xsz := XSz, ysz := YSz}} | Rest], Matrix) ->
+    
+    
+    
 
+read_matrix({X, Y}, Matrix) ->
+    Cols = lists:nth(Y, Matrix),
+    lists:nth(X, Cols).
+
+update_matrix(Rectangle, X, Y, Matrix) ->
+   %%TODO 
+
+get_rect({X,Y}, {XSz, YSz}, Matrix) ->
+    Rows = lists:sublist(Matrix, Y, YSz),
+    [lists:sublist(Row, X, XSz) || Row <- Rows].
+
+fill_rect([], Acc) -> lists:reverse(Acc);
+fill_rect([Row | Next], Acc) ->
+    fill_rect(Next, [fill(Row,<<>>) | Acc]).
+
+fill(<<"."/binary, Rest/binary>>, Acc) ->
+    fill(Rest, <<Acc/binary, "U"/binary>>);
+fill(<<"U"/binary, Rest/binary>>, Acc) ->
+    fill(Rest, <<Acc/binary, "X"/binary>>);
+fill(<<"X"/binary, Rest/binary, Acc) ->
+    fill(Rest, <<Acc/binary, "X"/binary>>);
+fill(<<>>, Acc) -> Acc.
+
+    
+    
